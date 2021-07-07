47ccd2b831a6616da125d1dc2a5058404976c300
Bolshakov Stanislav <bolshestas@gmail.com>
2021-07-07 23:15:22 +0300

    Add files via upload

---
+++ jsapp/index.js
@@ -0,0 +1,15 @@
+// const fruits = [
+//     {id: 1, title: 'Apple', price: 20, img: }
+//     {id: 2, title: 'Orange' price: 30 img: }
+//     {id: 3, title: 'Mango' price: 40, img: }
+// ]
+
+const modal = $.modal({
+    title: 'My modal!',
+    closable: true,
+    content: `
+        <p>Modal is working<p>
+        <p>Lorem ipsum dolor sit</p>
+    `,
+    width: '400px'
+})
---
+++ jsapp/plugins/modal.js
@@ -0,0 +1,54 @@
+function _createModal(options) {
+    const DEFAULT_WIDTH = '600px'
+    const modal = document.createElement('div')
+    modal.classList.add('vmodal')
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modal-ovelay" data-close=""true"">
+            <div class="modal-window" style="width: ${options.width || DEFAULT_WIDTH}">
+                <div class="modal-header">
+                    <span class="modal-title">${options.title || 'Окно'}</span>
+                    ${options.closable ? `<span class="modal-close" data-close="true">&times;</span>` : ''}
+                </div>
+                <div class="modal-body">
+                    ${options.content || ''}
+                </div>
+                <div class="modal-footer">
+                    <button>OK</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal)
+    return modal
+}
+
+$.modal = function(options) {
+    const ANIMATION_SPEED = 200
+    const $modal = _createModal(options)
+    let closing = false
+
+    const modal = {
+        open() {
+            !closing && $modal.classList.add('open')
+        },
+        close() {
+            closing = true
+            $modal.classList.remove('open')
+            $modal.classList.add('hide')
+            setTimeout(() => {
+                $modal.classList.add('hide')
+                closing = false
+            }, ANIMATION_SPEED)
+        },
+    }
+    
+    $modal.addEventListener('click', event => {
+        console.log('Clicked', event.target.dataset.close)
+        if(event.target.dataset.close) {
+            modal.close()
+        }
+    })
+
+    return modal
+}
